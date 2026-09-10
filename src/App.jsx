4ca03cfc03ee7574bd7fa0dac5ed320638412e67
kaifmohammad53 <kaifmohammad9530@gmail.com>
2026-09-10 18:22:33 +0530

    adding a new project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+      <h1 className='flex justify-center items-center text-6xl bg-slate-800 text-zinc-200'>Healix</h1>
+    </>
+  )
+}
+
+export default App

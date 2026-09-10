fb240adf8d2f792192fdabb332e71f4c0972ee62
kaifmohammad53 <kaifmohammad9530@gmail.com>
2026-09-10 20:28:55 +0530

    added hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
+import Hero from './Components/Hero'
 
 function App() {
   return (
     <>
-      <h1 className='flex justify-center items-center text-6xl bg-slate-800 text-zinc-200'>Healix</h1>
+    <Hero />
     </>
   )
 }
